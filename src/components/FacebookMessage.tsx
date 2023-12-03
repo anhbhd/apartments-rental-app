1fd2008b7f23a6d6060c4e53b00f45e36f7c3472
Anh bui <buihoangducanh0987@gmail.com>
2023-12-03 23:10:47 +0700

    Feature : UI - fix ui user

--- src/components/FacebookMessage.tsx
+++ src/components/FacebookMessage.tsx
@@ -1,9 +1,18 @@
 import { FacebookProvider, CustomChat } from "react-facebook";
+import { useLocation } from "react-router-dom";
+
 const FacebookMessage = () => {
+  const { pathname } = useLocation();
+  const prefixIsAdmin: boolean = pathname.split("/")[1] === "admin";
+
   return (
-    <FacebookProvider appId="362862773086452" chatSupport>
-      <CustomChat pageId="180645638465575" minimized={true} />
-    </FacebookProvider>
+    <div>
+      {!prefixIsAdmin ? (
+        <FacebookProvider appId="362862773086452" chatSupport>
+          <CustomChat pageId="180645638465575" minimized={true} />
+        </FacebookProvider>
+      ) : null}
+    </div>
   );
 };
 
