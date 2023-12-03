681044fdf3bf7d330893675d1b57f24a6fd60eec
Anh bui <buihoangducanh0987@gmail.com>
2023-12-03 13:57:18 +0700

    Feature: Add fb chat pluggin to production

--- src/components/FacebookMessage.tsx
+++ src/components/FacebookMessage.tsx
@@ -1,8 +1,8 @@
 import { FacebookProvider, CustomChat } from "react-facebook";
 const FacebookMessage = () => {
   return (
-    <FacebookProvider appId="665133312490429" chatSupport>
-      <CustomChat pageId="178963085299549" minimized={true} />
+    <FacebookProvider appId="362862773086452" chatSupport>
+      <CustomChat pageId="180645638465575" minimized={true} />
     </FacebookProvider>
   );
 };
